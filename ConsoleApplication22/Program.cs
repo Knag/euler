adc811e9a067ef09a8bbb9a3b53e28139c077fe9
Knag <erika.ekeberg@gmail.com>
2013-01-31 11:40:51 +0100

    22 completed

--- ConsoleApplication22/Program.cs
+++ ConsoleApplication22/Program.cs
@@ -13,6 +13,37 @@ namespace ConsoleApplication22
         {
             String line = "";
             string[] names;
+            List<string> alphabet = new List<string>();
+            System.Numerics.BigInteger biggest = 0;
+            int c = 1;
+
+            alphabet.Add("a");
+            alphabet.Add("b");
+            alphabet.Add("c");
+            alphabet.Add("d");
+            alphabet.Add("e"); // 5
+            alphabet.Add("f");
+            alphabet.Add("g");
+            alphabet.Add("h");
+            alphabet.Add("i");
+            alphabet.Add("j"); //10
+            alphabet.Add("k");
+            alphabet.Add("l");
+            alphabet.Add("m");
+            alphabet.Add("n");
+            alphabet.Add("o");
+            alphabet.Add("p");
+            alphabet.Add("q");
+            alphabet.Add("r");
+            alphabet.Add("s");
+            alphabet.Add("t"); // 20
+            alphabet.Add("u");
+            alphabet.Add("v");
+            alphabet.Add("w");
+            alphabet.Add("x");
+            alphabet.Add("y"); //25
+            alphabet.Add("z");
+
             try
             {
                 
@@ -22,21 +53,37 @@ namespace ConsoleApplication22
 
                 }
                 string sep = "\",\"";
-                names = Regex.Split(line, sep);
+                names = Regex.Split(line.ToLower(), sep);
                 Array.Sort(names);
-
+                foreach (var name in names)
+                {
+                    biggest += c * nameScore(name, alphabet);
+                    c++;
+                }
             }
             catch (Exception e)
             {
                 Console.WriteLine("The file could not be read:");
                 Console.WriteLine(e.Message);
             }
+
+ 
+            
+            nameScore("colin", alphabet);
+            Console.WriteLine(biggest);
             Console.Read();
         }
 
-        static int nameScore(string name)
-        {
 
+
+        static int nameScore(string name, List<string> alphabet)
+        {
+            int score = 0;
+            foreach(var c in name.ToCharArray())
+            {
+                score += alphabet.IndexOf(c.ToString())+1;
+            }
+            return score;
         }
     }
 }
